37018b33169c901dab5d022e87ae302e9ca276fe
mykebou <15079723+mykebou@users.noreply.github.com>
2018-08-18 14:13:02 +0100

    Gospelbuzz Footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<footer class="row">
 				<div class="full-width">
-					<span class="metatext footer-left">&copy; <a href="<?php echo get_permalink( get_page_by_title( 'About' ) ); ?>"><?php echo date("Y"); ?> Statesman Association Inc.</a></span>
-					<span class="metatext footer-right"><a href="http://www.sbstatesman.com/contact-us/">Contact Us</a> | <a href="<?php echo get_permalink( get_page_by_title( 'Donate' ) ); ?>">Donate</a> | <a href="http://www.sbstatesman.com/advertising/">Advertise</a> | <a href="http://www.sbstatesman.com/rack-locations/">Rack Locations</a> | <a href="mailto:web@sbstatesman.com">Site Feedback</a> | <a href="mailto:editors@sbstatesman.com">Send Us a Tip</a></span>
+					<span class="metatext footer-left">&copy; <a href="<?php echo get_permalink( get_page_by_title( 'About' ) ); ?>"><?php echo date("Y"); ?> Gospel Buzz Today!.</a></span>
+					<span class="metatext footer-right"><a href="http://gospelbuzz.net/contact-us/">Contact Us</a> | <a href="<?php echo get_permalink( get_page_by_title( 'Donate' ) ); ?>">Donate</a> | <a href="http://gospelbuzz.net/advertising/">Advertise</a> | <a href="http://gospelbuzz.net/rack-locations/">Rack Locations</a> | <a href="mailto:info@gospelbuzz.net/">Site Feedback</a> | <a href="mailto:info@gospelbuzz.net">Send Us a Tip</a></span>
 				</div>
 			</footer>
 		</div>
